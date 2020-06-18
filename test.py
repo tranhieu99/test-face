0a3b3649e2f7eef4efb73810005382445604c43f
hieutt <hieutt1348@gmail.com>
2020-06-17 20:08:33 -0700

    2 commit

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
-p = 5 
-print(p)
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+video = cv2.VideoCapture("rtsp://admin:Polyco6688@192.168.1.154")
+
+while True:
+
+    _, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow("RTSP", frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
